f6424ee33a07afcba58443cebdea02f007405708
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2017-06-09 09:33:16 +0200

    part 2

--- service/src/main/scala/com/softwaremill/sandbox/MainModule.scala
+++ service/src/main/scala/com/softwaremill/sandbox/MainModule.scala
@@ -1,35 +1,28 @@
 package com.softwaremill.sandbox
 
-import java.util.UUID
-
-import akka.actor.Status.Status
-import akka.actor.{ActorSystem, Props, Status}
+import akka.actor.{ActorSystem, Props}
 import akka.cluster.sharding.{ClusterSharding, ClusterShardingSettings}
 import akka.event.Logging.LogLevel
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.{DebuggingDirectives, LogEntry, LoggingMagnet}
 import akka.stream.{ActorMaterializer, Materializer}
-import akka.util.Timeout
 import ch.megard.akka.http.cors.CorsDirectives
 import com.softwaremill.macwire._
+import com.softwaremill.sandbox.api.http.{ApiConfig, UserController}
+import com.softwaremill.sandbox.application.UserActor.{UserRegion, UserRegionTag}
+import com.softwaremill.sandbox.application.{UserActor, UserActorConfig, UserActorMessageExtractor, UserService}
+import com.softwaremill.tagging.Tagger
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.util.{Failure, Random, Success}
-import akka.pattern._
-import com.softwaremill.sandbox.api.http.{ApiConfig, UserController}
-import com.softwaremill.sandbox.application.{UserActor, UserActorMessageExtractor}
-import com.softwaremill.sandbox.application.UserActor.{UserRegion, UserRegionTag}
-import com.softwaremill.tagging.Tagger
-import kamon.trace.Tracer
+import scala.util.{Failure, Success}
 
 trait MainModule extends LazyLogging with CorsDirectives {
   def config: Config
@@ -39,17 +32,19 @@ trait MainModule extends LazyLogging with CorsDirectives {
   private var serverBinding: Option[ServerBinding] = None
   private lazy val apiConfig = wire[ApiConfig]
   private lazy val userActorMessageExtractor = wire[UserActorMessageExtractor]
+  private lazy val userActorConfig = wire[UserActorConfig]
 
   private lazy val userRegion: UserRegion =
     ClusterSharding(system)
       .start(
         typeName = "UserActor",
-        entityProps = Props[UserActor],
+        entityProps = Props(wire[UserActor]),
         settings = ClusterShardingSettings(system),
         messageExtractor = userActorMessageExtractor
       )
       .taggedWith[UserRegionTag]
 
+  private lazy val userService = wire[UserService]
   private lazy val userController = wire[UserController]
 
   def startApi(): Future[ServerBinding] = {
