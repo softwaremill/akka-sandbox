dd8e19ed5963a663f5cf277ae6f35dd482aad7fe
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2017-06-09 08:29:13 +0200

    filters

--- perf-test/src/test/scala/com/softwaremill/sandbox/UserCreationInClusterSimulation.scala
+++ perf-test/src/test/scala/com/softwaremill/sandbox/UserCreationInClusterSimulation.scala
@@ -9,10 +9,10 @@ class UserCreationInClusterSimulation extends BaseSimulation {
 
   setUp(
     scenario("User creation host1").exec(userCreationChain)
-      .inject(rampUsers(500) over(240 seconds) )
+      .inject(rampUsers(1500) over(240 seconds) )
       .protocols(host1HttpConf),
     scenario("User creation host2").exec(userCreationChain)
-      .inject(rampUsers(500) over(240 seconds) )
+      .inject(rampUsers(1500) over(240 seconds) )
       .protocols(host2HttpConf))
     .assertions(forAll.failedRequests.percent.is(0))
 }
