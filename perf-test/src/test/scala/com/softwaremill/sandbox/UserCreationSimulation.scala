7653d368c3d5b4a1dba0c8b502738af429a03e79
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2017-06-12 08:54:23 +0200

    configuration

--- perf-test/src/test/scala/com/softwaremill/sandbox/UserCreationSimulation.scala
+++ perf-test/src/test/scala/com/softwaremill/sandbox/UserCreationSimulation.scala
@@ -9,7 +9,7 @@ class UserCreationSimulation extends BaseSimulation {
 
   setUp(
     scenario("User creation").exec(userCreationChain)
-      .inject(rampUsers(1) over (120 seconds))
+      .inject(rampUsers(100) over (120 seconds))
       .protocols(httpConf))
     .assertions(forAll.failedRequests.percent.is(0))
 }
